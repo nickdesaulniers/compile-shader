bb88ac0ff218a2bbc6b2cadcf4775c242b5bff9f
Nick Desaulniers <ndesaulniers@mozilla.com>
2015-09-07 20:13:49 -0700

    improve tests

--- test/test.js
+++ test/test.js
@@ -1,18 +1,40 @@
 var test = require('tape');
 var shaderCompiler = require('..');
 
+function getGL () {
+  return document.createElement('canvas').getContext('webgl');
+};
+
 test('bad input', function (t) {
-  t.plan(1);
+  t.plan(3);
   t.throws(function () {
     shaderCompiler(null, null, null);
+  }, 'all null arguments');
+  t.throws(function () {
+    shaderCompiler(null, WebGLRenderingContext.prototype.VERTEX_SHADER, null);
+  }, 'correct type, rest null');
+  t.throws(function () {
+    var gl = getGL();
+    var type = gl.VERTEX_SHADER;
+    var src = '';
+    shaderCompiler(gl, type, src);
   });
 });
 
-test('good input', function (t) {
+test('good input, vertex shader', function (t) {
   t.plan(1);
-  var gl = document.createElement('canvas').getContext('webgl');
+  var gl = getGL();
   var type = gl.VERTEX_SHADER;
-  var src = "void main () { gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
+  var src = 'void main () { gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }';
+  var shader = shaderCompiler(gl, type, src);
+  t.ok(shader instanceof WebGLShader, 'valid shader');
+});
+
+test('good input, fragment shader', function (t) {
+  t.plan(1);
+  var gl = getGL();
+  var type = gl.FRAGMENT_SHADER;
+  var src = 'precision mediump float; void main () { gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0); }';
   var shader = shaderCompiler(gl, type, src);
   t.ok(shader instanceof WebGLShader, 'valid shader');
 });
